b6a9be7a4b90d1ce5820000ddd4400971908191e
Frenk <frk.grd@libero.it>
2019-05-21 00:48:34 +0200

    initial commit

---
+++ es3.m
@@ -0,0 +1,41 @@
+N :=1371369341274601948778278636423406690031517505123926008773053567592011624152780573861293737219;
+e :=14613992044615514362873325722734275801042498633752586509535659216428873147567136300337933519;
+
+NFact := Factorization(N);
+pExp := NFact[1];
+qExp := NFact[2];
+q := qExp[1];
+p := pExp[1];
+FiN := (p-1)*(q-1);
+d := Modinv(e, FiN);
+printf "p = ";
+p;
+printf "q = ";
+q;
+printf "d = ";
+d;
+printf "\n";
+if p*q eq N then
+  printf "p*q fa n. DAGLIE!\n";
+else
+  printf "p*q non fa n. GIM DUELLE\n";
+end if;
+if e eq Modinv(d, FiN) then
+  printf "d e' l'inverso di e. GOOD.\n";
+else
+  printf "d non e' l'inverso di e.\n";
+end if;
+
+SetOutputFile("C:/Users/Frenk/Desktop/MAGISTRALE/Crittografia e Applicazioni/CryptoWars/es3.txt": Overwrite := true);
+PrintFile("C:/Users/Frenk/Desktop/MAGISTRALE/Crittografia e Applicazioni/CryptoWars/es3.txt", "p = " cat IntegerToString(p));
+PrintFile("C:/Users/Frenk/Desktop/MAGISTRALE/Crittografia e Applicazioni/CryptoWars/es3.txt", "q = " cat IntegerToString(q));
+PrintFile("C:/Users/Frenk/Desktop/MAGISTRALE/Crittografia e Applicazioni/CryptoWars/es3.txt", "d = " cat IntegerToString(d));
+UnsetOutputFile();
+
+
+/* Factorization(N);
+FiN := N-1;
+a := 19;
+if a ne FiN then
+  a;
+end if; */
